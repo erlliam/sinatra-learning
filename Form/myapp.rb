96212496de06f46c6cc5a8664e8494aa1f5aa776
q <q>
2018-04-16 22:49:31 -0400

    datamapper sqlite3 added

--- Form/myapp.rb
+++ Form/myapp.rb
@@ -1,11 +1,31 @@
 # myapp.rb
 require "sinatra"
+require "data_mapper"
+
+DataMapper.setup(:default, "sqlite::memory:")
 
 get "/" do
 	File.read("index.html")
 end
 
 post "/" do
-	"#{params["inputOne"]}"
+	@post = Post.create(
+		:title => "My first DataMapper post",
+		:body => "#{params["inputOne"]}",
+		:created_at => Time.now
+		)
+	"#{Post.get(1).title}, #{Post.get(1).body}, #{Post.get(1).created_at}"
 end
 
+class Post
+	include DataMapper::Resource
+
+	property :id, Serial
+	property :title, String
+	property :body, Text
+	property :created_at, DateTime
+
+end
+
+DataMapper.finalize
+DataMapper.auto_migrate!
